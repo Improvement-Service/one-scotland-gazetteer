37b52e94dd9bd71f2468e37259f1b73cb0021c1a
dmichelakis <Dimitrios.Michelakis@improvementservice.org.uk>
2018-03-14 11:46:16 +0000

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -6,7 +6,6 @@ class Conf:
 
     def __init__(self):
 
-        self.ftp_download_folder = 'DOWNLOAD'
         self.ftp_host = '213.128.245.37'
         self.ftp_user = 'your-user'
         self.ftp_pass = 'your-pass'
