10abf62911e7e13c35754b762a8480fc30f03af3
dmichelakis <Dimitrios.Michelakis@improvementservice.org.uk>
2018-03-14 11:27:52 +0000

    Create the configuration file
    Includes all configuration needed to support connecting to the One Scotland Gazetteer (i.e. user, password, port, and IP) and any other data needed to support downloading files.

---
+++ configuration.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+
+class Conf:
+
+    def __init__(self):
+
+        self.ftp_download_folder = 'DOWNLOAD'
+        self.ftp_host = '213.128.245.37'
+        self.ftp_user = 'your-user'
+        self.ftp_pass = 'your-pass'
+        self.ftp_port = 990
